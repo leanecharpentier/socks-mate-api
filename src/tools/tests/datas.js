224f6237cd37f3122939b47e188184a3d04b8b22
leanecharpentier <leanecharpentierpro@outlook.com>
2024-09-12 16:57:15 +0200

    demo

--- src/tools/tests/datas.js
+++ src/tools/tests/datas.js
@@ -4,7 +4,7 @@ export class UserBuilder {
     constructor() {
         this.username = faker.internet.userName();
         this.password = faker.internet.password();
-        this.biography = faker.lorem.text();
+        this.biography = faker.lorem.word({ length: { min: 10, max: 25 }, strategy: 'fail' });
         this.size = faker.number.int({ min: 35, max: 48 });
         this.urlImage = "https://lh5.googleusercontent.com/proxy/qLv8SoOi7odqcIveS66RiDdIdYtpo-OF1-9nHQK6vGIrIDzAY539oYyvijNy5o0nFZshoXZoMRK0rW8JqEc3RgwKGM5_jBGVASnRGZMG9O_g"
     }
