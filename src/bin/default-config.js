5be0f695953596768a787671345b84c5272cd234
leanecharpentier <leanecharpentierpro@outlook.com>
2024-09-12 16:52:19 +0200

    config

--- src/bin/default-config.js
+++ src/bin/default-config.js
@@ -1,8 +1,9 @@
 function init() {
     return new Promise((resolve) => {
         console.log('default config');
-        process.env.MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://leanecharpentier:ZJuEnadLTKugad44@cluster0.f3skjix.mongodb.net/socksmate-prod"
-        process.env.PORT = process.env.PORT || 3000;
+        process.env.MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://leanecharpentier:ZJuEnadLTKugad44@cluster0.f3skjix.mongodb.net/socksmate-dev"
+        process.env.DB_NAME = "socksmate-dev"
+        process.env.PORT = process.env.PORT || 3000
         process.env.SECRET_KEY = process.env.SECRET_KEY || "secretKey"
         return resolve('ok');
     })
